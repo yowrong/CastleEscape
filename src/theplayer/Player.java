a87c3b60857b1c73d01b574c43ebb912f610f94d
Kelvin Ngo <kelvinngosos@gmail.com>
2021-03-13 14:52:02 -0800

    added more Player methods

--- src/theplayer/Player.java
+++ src/theplayer/Player.java
@@ -39,6 +39,8 @@ public class Player {
     }
 
     private void playerMove(String direction) {
+        int[] playerOldPos = this.playerCoordinates;
+
         if (direction == "north") {
             this.playerCoordinates[0] -= 1;
         }
@@ -54,6 +56,8 @@ public class Player {
         else if (direction == "east") {
             this.playerCoordinates[1] += 1;
         }
+
+        currentRoom.updatePlayerPos(playerOldPos, this.playerCoordinates);
     }
 
     private void placeItem(String itemName) {
@@ -62,7 +66,15 @@ public class Player {
                 itemInInventory.setItemCoordinate(this.getPlayerCoordinates());
                 currentRoom.addItemToRoom(itemInInventory, itemInInventory.getItemCoordinate());
                 inventory.remove(itemInInventory);
+            }
+        }
+    }
 
+    private void pickUpItem() {
+        for (Item itemInRoom : currentRoom.getItemsInRoom()) {
+            if (this.playerCoordinates == itemInRoom.getItemCoordinate()) {
+                this.getInventory().add(itemInRoom);
+                currentRoom.getItemsInRoom().remove(itemInRoom);
             }
         }
     }
