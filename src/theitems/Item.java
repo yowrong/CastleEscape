4612a956e27d52924a6bfb149d771c45c7caf391
Kelvin Ngo <kelvinngosos@gmail.com>
2021-03-13 13:32:54 -0800

    Worked on Player and Obstacle

--- src/theitems/Item.java
+++ src/theitems/Item.java
@@ -2,9 +2,9 @@ package theitems;
 
 public class Item {
 
-    private int[] itemCoordinate;
     private String itemName;
     private String itemDescription;
+    private int[] itemCoordinate;
 
     public Item(int[] itemCoordinate, String itemName, String itemDescription) {
         this.itemCoordinate = itemCoordinate;
