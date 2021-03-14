f1e24e5e66538e7b22d28cf5af92f1b530e056fd
madtv129 <madtv129@gmail.com>
2021-03-13 16:52:02 -0800

    Merge branch 'main' of https://github.com/yowrong/EscapeRoom into main
    # Conflicts:
    #	out/production/EscapeRoom/rooms/Room2.class

--- src/rooms/StartingRoom.java
+++ src/rooms/StartingRoom.java
@@ -1,6 +1,7 @@
 package rooms;
 
 import theitems.Item;
+import theplayer.Player;
 
 import java.util.ArrayList;
 
@@ -8,6 +9,8 @@ public class StartingRoom extends Room {
     private int[][] exitCoordinate;
     private ArrayList<Item> itemsInRoom;
     private String[][] StartingRoom;
+    private int[] torchPlace = {1, 5};
+
 
     public StartingRoom(int[][] exitCoordinates, ArrayList<Item> items, String[][] map) {
         super(exitCoordinates, items, map);
@@ -27,32 +30,47 @@ public class StartingRoom extends Room {
         }
     }
 
-    protected void addItemToRoom(Item itemToDrop, int[] currentPosition) {
+
+    public void addItemToRoom(Item itemToDrop, int[] currentPosition) {
         this.itemsInRoom.add(itemToDrop);
         itemToDrop.setItemCoordinate(currentPosition);
     }
-
 
     protected void populateRoom() {
         for (Item torch : itemsInRoom) {
             this.StartingRoom[torch.getItemCoordinate()[0]][torch.getItemCoordinate()[1]] = "T";
         }
         this.getExitCoordinate();
+
+        //Real door
         this.StartingRoom[this.getExitCoordinate()[0][0]][this.getExitCoordinate()[0][1]] = "D";
 
 //        this.StartingRoom[(this.getExitCoordinate()[0][0]) + 1][this.getExitCoordinate()[0][1]] = "P";
 
         //EXPERIMENTAL -- This is where player spawns, Door behind starting point should be dysfunctional.
         //User should be prompted something along the lines "door is shut with a magical force"
+
+        //Locked door
         this.StartingRoom[(this.getExitCoordinate()[0][0]) + 4][this.getExitCoordinate()[0][1] - 4] = "D";
         this.StartingRoom[(this.getExitCoordinate()[0][0]) + 4][this.getExitCoordinate()[0][1] - 3] = "P";
+        this.StartingRoom[this.getExitCoordinate()[0][0] + 1][this.getExitCoordinate()[0][1] + 1] = "H";
     }
+
+    public void torchLever(Player player) {
+        for (Item itemInInventory : player.getInventory()) {
+
+            if (player.getPlayerCoordinates() == torchPlace && itemInInventory.getItemName() == "torch") {
+                //Player places item.
+            }
+        }
+    }
+
 
     public static void main(final String[] args) {
         int[][] testExitCoord = {{0, 4}};
         int[] testItemCoord = {5, 3};
         String testItemName = "A torch";
-        String testItemDesc = "I am a torch.";
+        String testItemDesc = "It's a torch... Seems to light the way";
 
         Item torch = new Item(testItemCoord, testItemName, testItemDesc);
         ArrayList<Item> testItemsInRoom = new ArrayList<Item>();
@@ -74,6 +92,8 @@ public class StartingRoom extends Room {
 //        testStartingRoom.displayLayout();
         testStartingRoom.populateRoom();
 
+        //Room desc
+        System.out.println();
 
         testStartingRoom.displayLayout();
 
