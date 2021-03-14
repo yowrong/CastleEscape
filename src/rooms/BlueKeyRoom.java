1e6dab1f0d19801c6bfdc239f6509c7dfe283d35
Andy Chang <ychang118@my.bcit.ca>
2021-03-14 04:59:54 -0700

    update

--- src/rooms/BlueKeyRoom.java
+++ src/rooms/BlueKeyRoom.java
@@ -13,6 +13,8 @@ public class BlueKeyRoom extends Room {
     private Obstacle obstacleInRoom;
     private int[] pressPlate = {4, 4};
     private int[] table = {5, 1};
+    private boolean wallsOpened = false;
+    private boolean eventTrigger;
 
     public BlueKeyRoom(int[][] exitCoordinates, ArrayList<Item> items, String[][] map, Obstacle obstacleInRoom) {
         super(exitCoordinates, items, map);
@@ -33,9 +35,6 @@ public class BlueKeyRoom extends Room {
     //populateRoom creates the room
     @Override
     protected void populateRoom() {
-//        for (Item item : this.getItemsInRoom()) {
-//            this.getMap()[item.getItemCoordinate()[0]][item.getItemCoordinate()[1]] = "?";
-//        }
         this.getMap()[getExitCoordinate()[0][0]][getExitCoordinate()[0][1]] = "D";
         this.getMap()[getExitCoordinate()[0][0]][(getExitCoordinate()[0][1]-1)] = "P";
         this.getMap()[obstacleInRoom.getObstacleCoordinate()[0]][obstacleInRoom.getObstacleCoordinate()[1]] = "O";
@@ -44,104 +43,87 @@ public class BlueKeyRoom extends Room {
     }
 
     @Override
-    public void checkEventTriggers() {
-        boolean eventTrigger = false;
-        if (this.onPressPlate()) {
-            eventTrigger = true;
+    public void exitRoom(int[] playerCoord, Player thePlayer, Room[] nextRoom) {
+        if (playerCoord[0] == 4 && playerCoord[1] == 5){
+            thePlayer.setCurrentRoom(nextRoom[4]);
+        } else {
+            System.out.println("You are not close enough to the door!");
         }
     }
 
     @Override
-    public void exitRoom(int[] playerCoord, Player thePlayer, Room[] nextRoom) {
-        for (int i = 0; i < this.getExitCoordinate().length; i++) {
-            for (int j = 0; j < this.getExitCoordinate()[i].length; j++) {
-                if ((Math.abs(playerCoord[0] - this.getExitCoordinate()[i][j]) == 1
-                        && Math.abs(playerCoord[1] - this.getExitCoordinate()[i][j]) == 0)
-                        || (Math.abs(playerCoord[0] - this.getExitCoordinate()[i][j]) == 0
-                        && Math.abs(playerCoord[1] - this.getExitCoordinate()[i][j]) == 1)) {
-                    if (Arrays.equals(this.getExitCoordinate()[i], new int[]{2, 1})) {
-                        thePlayer.setCurrentRoom(nextRoom[0]);
-                    } else if (Arrays.equals(this.getExitCoordinate()[i], new int[]{2, 5})) {
-                        thePlayer.setCurrentRoom(nextRoom[1]);
-                    } else if (Arrays.equals(this.getExitCoordinate()[i], new int[]{4, 5})) {
-                        thePlayer.setCurrentRoom(nextRoom[2]);
-                    } else if (Arrays.equals(this.getExitCoordinate()[i], new int[]{4, 1})) {
-                        thePlayer.setCurrentRoom(nextRoom[3]);
-                    } else {
-                        thePlayer.setCurrentRoom(nextRoom[4]);
-                    }
-                } else {
-                    System.out.println("You are not close enough to the door!");
-                }
-            }
-        }
+    public void checkEventTriggers() {
+        this.eventTrigger = this.onPressPlate();
     }
 
-    //onPressPlate when the player or obstacle is on the pressure plate, the table item becomes accessible
     public boolean onPressPlate() {
         if (this.getMap()[pressPlate[0]][pressPlate[1]].equals("P")
                 || this.getMap()[pressPlate[0]][pressPlate[1]].equals("O")) {
-            System.out.println("You see a table with jewels scattered on top at the back of the room");
+            if (!this.wallsOpened) {
+                System.out.println("The wall behind the table opens revealing a hidden chest.");
+                this.wallsOpened = true;
+            }
             return true;
         } else {
-            System.out.println("There's a table at the back of the room, but you can't make out what is on the table.");
+            if (this.wallsOpened) {
+                System.out.println("The wall closes around the hidden chest.");
+                this.wallsOpened = false;
+            }
             return false;
         }
     }
 
-    //generateBigKey generates the Big Blue Key once the player has solved the puzzle
-    private void generateBigKey(Player player) {
-        int[] blueKeyCoord = {player.getPlayerCoordinates()[0],player.getPlayerCoordinates()[1]};
+    private void generateBigKey(Player thePlayer) {
+        int[] blueKeyCoord = {thePlayer.getPlayerCoordinates()[0], thePlayer.getPlayerCoordinates()[1]};
         Item bigBlueKey = new Item(blueKeyCoord, "Big Blue Key", "A large shiny blue key");
         this.getItemsInRoom().add(bigBlueKey);
         this.getMap()[blueKeyCoord[0]][blueKeyCoord[1]] = "!";
     }
 
-    //jewelSort is a sequence puzzle triggered when the player is in the adjacent position to the table item
-    public void jewelSort(Player player) {
+    public void jewelSort(Player thePlayer) {
         String chestSlots = "ruby emerald sapphire";
         Scanner scan = new Scanner(System.in);
 
         int[] tableAccessPt1 = {table[0], table[1]+1};
         int[] tableAccessPt2 = {table[0]-1, table[1]};
 
-        if (Arrays.equals(player.getPlayerCoordinates(), tableAccessPt1)
-                || Arrays.equals(player.getPlayerCoordinates(), tableAccessPt2)) {
-            System.out.println("There are three jewels on the table, a ruby, a sapphire, and an emerald."
-                + "\nYou notice there is a chest with 3 colored slots in the order: red, green, blue"
-                + "\nEnter the order you place the jewels in to unlock the chest:");
+        if (Arrays.equals(thePlayer.getPlayerCoordinates(), tableAccessPt1)
+                || Arrays.equals(thePlayer.getPlayerCoordinates(), tableAccessPt2)) {
+            System.out.println("There are three jewels on the table, a sapphire, a ruby, and an emerald."
+                + "\nYou notice the chest has 3 colored slots in the order: red, green, and blue"
+                + "\nEnter the order (without commas), to place the jewels in, to unlock the chest:");
             while (!scan.nextLine().equals(chestSlots)) {
                     System.out.println("The jewels did not fit into the chest, try again?");
             }
             System.out.println("The jewels clicked into the chest. The chest opens and reveals a key.");
-            generateBigKey(player);
+            generateBigKey(thePlayer);
         }
         scan.close();
     }
 
 
 
-    public static void main(final String[] args) {
-        int[][] testExitCoord = {{4, 6}};
-        int[] testItemCoord = {5, 1};
+//    public static void main(final String[] args) {
+//        int[][] testExitCoord = {{4, 6}};
+//        int[] testItemCoord = {5, 1};
 //        String testItemName = "Table";
 //        String testItemDesc = "A table";
-        int[] testObsCoord = {1, 4};
-        int[] testPlayCoord = {5, 2};
-
-        Obstacle testObstacle = new Obstacle("crate", "I'm a crate", testObsCoord);
-
+//        int[] testObsCoord = {1, 4};
+//        int[] testPlayCoord = {5, 2};
+//
+//        Obstacle testObstacle = new Obstacle("crate", "I'm a crate", testObsCoord);
+//
 //        Item testItem = new Item(testItemCoord, testItemName, testItemDesc);
-
-        ArrayList<Item> testItemsInRoom = new ArrayList<>();
+//
+//        ArrayList<Item> testItemsInRoom = new ArrayList<>();
 //        testItemsInRoom.add(testItem);
-        String[][] testMap = new String[7][7];
-        for (int index = 0; index < 7; index++) {
-            for (int innerIndex = 0; innerIndex < 7; innerIndex++) {
-                testMap[index][innerIndex] = "U";
-            }
-        }
-        BlueKeyRoom testRoom = new BlueKeyRoom(testExitCoord, testItemsInRoom, testMap, testObstacle);
+//        String[][] testMap = new String[7][7];
+//        for (int index = 0; index < 7; index++) {
+//            for (int innerIndex = 0; innerIndex < 7; innerIndex++) {
+//                testMap[index][innerIndex] = "U";
+//            }
+//        }
+//        BlueKeyRoom testRoom = new BlueKeyRoom(testExitCoord, testItemsInRoom, testMap, testObstacle);
 
 //        Player testPlayer = new Player("Player", testRoom, testPlayCoord);
 
@@ -193,8 +175,8 @@ public class BlueKeyRoom extends Room {
 //                System.out.println("The jewels did not fit into the chest, try again?");
 //            }
 //        }
+//    }
 
-    }
 
 
 }
