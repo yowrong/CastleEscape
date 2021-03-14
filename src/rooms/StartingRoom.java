a80d5acc19cc461a7481fc2570ede06d72519f91
Kelvin Ngo <kelvinngosos@gmail.com>
2021-03-14 06:23:49 -0700

    bro

--- src/rooms/StartingRoom.java
+++ src/rooms/StartingRoom.java
@@ -26,7 +26,7 @@ public class StartingRoom extends Room {
         }
     }
     @Override
-    protected void populateRoom() {
+    protected void populateRoom(Player thePlayer) {
         // Might need to change itemsInRoom to this.itemsInRoom
         this.getMap()[this.pressPlate[0]][this.pressPlate[1]] = "*";
         for (Item torch : this.getItemsInRoom()) {
@@ -121,7 +121,7 @@ public class StartingRoom extends Room {
         testStartingRoom.createLayout();
         testStartingRoom.generateTorch();
         testStartingRoom.generateLockedDoor();
-        testStartingRoom.populateRoom();
+//        testStartingRoom.populateRoom();
 
         //Room desc
 
@@ -142,7 +142,7 @@ public class StartingRoom extends Room {
         testStartingRoom.checkEventTriggers();
         System.out.println(testStartingRoom.onPressPlate());
         testStartingRoom.createLayout();
-        testStartingRoom.populateRoom();
+//        testStartingRoom.populateRoom();
         testStartingRoom.displayLayout();
 
 
