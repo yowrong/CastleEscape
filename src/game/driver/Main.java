dfc8cf3a1ced7ff2932088e0e6c66a71d5050c12
Andy Chang <ychang118@my.bcit.ca>
2021-03-14 00:01:41 -0800

    added bluekeyroom to main

--- src/game/driver/Main.java
+++ src/game/driver/Main.java
@@ -1,13 +1,33 @@
 package game.driver;
 
+import rooms.BlueKeyRoom;
 import theitems.Item;
+import theobstacles.Obstacle;
+import theplayer.Player;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[][] blueRmExitCoord = {{4, 6}};
+        int[] blueRmObsCoord = {1, 4};
+        ArrayList<Item> blueRmItems = new ArrayList<>();
+
+        int[] playerCoord = {blueRmExitCoord[0][0], blueRmExitCoord[0][1]};
+
+        String[][] map = new String[7][7];
+        for (int index = 0; index < 7; index++) {
+            for (int innerIndex = 0; innerIndex < 7; innerIndex++) {
+                map[index][innerIndex] = "U";
+            }
+        }
+        Obstacle blueRmObstacle = new Obstacle("Crate", "A moveable crate", blueRmObsCoord);
+
+        BlueKeyRoom blueKeyRoom = new BlueKeyRoom(blueRmExitCoord, blueRmItems, map, blueRmObstacle);
+        Player player = new Player("Player", blueKeyRoom, playerCoord);
+
+        // write your code here
         // create an item with its own coordinate
         // put item in list
         // create room and pass list
